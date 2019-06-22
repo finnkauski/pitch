3554ac0c204557e5bcf8660fb222f7165c1ab8f5
Finnkauski <iwiivi@gmail.com>
2019-06-22 15:57:57 +0100

    make the basic template. next: url revealjs

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -2,6 +2,7 @@
 
 #[macro_use]
 extern crate rocket;
+extern crate pandoc;
 extern crate rocket_contrib;
 
 use elevator_pitch::{proj_io::write_out, UserInput};
@@ -11,23 +12,47 @@ use rocket_contrib::templates::tera::Tera;
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
 
+// pandoc
+
 #[get("/")]
 fn home() -> Template {
     let context: HashMap<i8, i8> = HashMap::new();
     Template::render("index", context)
 }
 
+pub fn process_form(input: UserInput) {
+    // deal with form
+    let t = Tera::new("templates/**/*").unwrap();
+    let rendered = t.render("project.md.tera", &input).unwrap();
+
+    write_out(&rendered, "static/outputs/output.md").expect("Failed to render template!");
+    convert(&rendered, "static/outputs/test.html".to_owned())
+}
+
+pub fn convert(input: &String, output_path: String) {
+    let mut p = pandoc::new();
+    p.set_input_format(pandoc::InputFormat::MarkdownGithub, vec![]);
+    p.set_input(pandoc::InputKind::Pipe(input.to_string()));
+
+    p.set_output_format(pandoc::OutputFormat::Revealjs, vec![]);
+    p.add_options(&vec![
+        pandoc::PandocOption::Standalone,
+        pandoc::PandocOption::SlideLevel(2),
+    ]);
+    p.set_output(pandoc::OutputKind::File(output_path));
+
+    match p.execute() {
+        Ok(_) => println!("Form processed and converted!"),
+        Err(_) => println!("Could not process form!"),
+    }
+}
+
 #[post("/", data = "<mainform>")]
 fn handle_form(mainform: Form<UserInput>) -> Template {
-    let context: HashMap<i8, i8> = HashMap::new();
-    let t = Tera::new("templates/**/*").unwrap();
-    write_out(
-        &t.render("project.html.tera", &mainform.into_inner())
-            .unwrap(),
-        "static/outputs/output.html",
-    )
-    .expect("Failed to render template!");
+    process_form(mainform.into_inner());
 
+    // return main home page
+    let context: HashMap<i8, i8> = HashMap::new();
     Template::render("index", context)
 }
 
